278a2977d13641b57a1cb6d27078afe9655fd1f8
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-03-25 10:01:36 +0100

    Add files via upload

--- src/be/intecbrussel/examples/Examples.java
+++ src/be/intecbrussel/examples/Examples.java
@@ -1,5 +1,6 @@
 package be.intecbrussel.examples;
 
+import java.util.OptionalInt;
 import java.util.function.Consumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -9,7 +10,7 @@ public class Examples {
         String[] words = {"Hello", "this", "is", "an", "example", "of", "streams"};
         Stream.of(words)
                 .forEach(System.out::println);
-       // Stream<String> stream = Stream.of(words);
+        // Stream<String> stream = Stream.of(words);
         //Consumer<String>consumer = System.out::println;
         //stream.forEach(consumer);
 
@@ -19,25 +20,55 @@ public class Examples {
         IntStream iStream = IntStream.rangeClosed(0, 10);
         //iStream.forEach(System.out::println);
 
-        IntStream.rangeClosed(11,20);
-               // .forEach(System.out::println);
+        IntStream.rangeClosed(11, 20);
+        // .forEach(System.out::println);
 
-       IntStream.iterate(0, i -> i+1);
-                //.forEach(System.out::println);
+        IntStream.iterate(0, i -> i + 1);
+        //.forEach(System.out::println);
 
         String[] howdy = {"Howdy", "we", "come", "in", "peace"};
         Stream.of(howdy).forEach(System.out::println);
 
         IntStream.rangeClosed(0, 99);
-                //.forEach(System.out::println);
+        //.forEach(System.out::println);
 
+        //using stream to reduce data
+        //sum of int in range
         int sum = IntStream.rangeClosed(0, 10)
                 .sum();
         System.out.println(sum);
 
+        //max of int in range
+        OptionalInt max = IntStream.rangeClosed(0, 10)
+                .max();
+
+        if (max.isPresent()) {
+            System.out.println(max.getAsInt());
+        } else {
+            System.out.println("No result");
         }
 
+        //stream without elements
+        OptionalInt max1 = IntStream.of()
+                .max();
+
+        if(max1.isPresent()){
+            System.out.println(max1.getAsInt());
+        } else {
+            System.out.println("No result");
         }
+
+        //use of reduce method to create own reduction method
+        int result = IntStream.rangeClosed(1,10)
+                .reduce(1,(acc, el) -> acc*el);
+        System.out.println(result);
+
+        //use of BinarryOperator <T> to reduce other data types
+        String res = Stream.of(words)
+                .reduce("*", (acc, el) -> acc + el + "*");
+        System.out.println(res);
+    }
+}
 
 
 
